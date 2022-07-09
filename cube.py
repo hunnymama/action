154f76189b38f8dc5f32847a78779e148edfb050
Kaïn <107337296+hunnymama@users.noreply.github.com>
2022-07-09 12:26:47 -0700

    Add files via upload
    CUBE PYTHON DRAWING

---
+++ cube.py
@@ -0,0 +1,84 @@
+
+# First lets import turtle module for python
+import turtle
+
+#speed
+turtle.speed(5)
+
+# Now lets draw a line (step one)
+turtle.forward(100)
+
+# The 100 is for the length of the line
+
+# Now we change the direction of the arrow 90 Degrees
+turtle.right(90)
+
+# Now lets have it go forward another 100 (step two)
+turtle.forward(100)
+
+#change degrees
+turtle.right(90)
+
+#goin fwd :)
+turtle.forward(100)
+
+#change degrees
+turtle.right(90)
+
+#fwd
+turtle.forward(100)
+
+#diagonal change degree
+turtle.right(45)
+
+#go
+turtle.forward(50)
+
+#change degrees
+turtle.right(45)
+
+#go
+turtle.forward(100)
+
+#same
+turtle.right(135)
+
+#fwd
+turtle.forward(50)
+
+#change degrees 
+turtle.right(180)
+
+#fwd
+turtle.forward(50)
+
+#change degrees 
+turtle.right(135)
+
+#fwd same distance as square length
+turtle.forward(100)
+
+#change degrees 
+turtle.right(45)
+
+#same ANGLE
+turtle.forward(50)
+# Now we want to run the code and have it showing until we decide to close it
+turtle.done()
+
+
+#turtle.fd(distance) instead of forward
+#turtle.rt = rigt
+#turtle.lt = left
+#check out protractor for angles
+#turtle.speed(0)
+
+
+ #If input is a number greater than 10 or smaller than 0.5, speed is set to 0. Speedstrings are mapped to speedvalues as follows:
+
+#fastest: 0
+#fast: 10
+#normal: 6
+#slow: 3
+#slowest: 1
+    
